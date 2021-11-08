a6038b139fa95f20eb725b84a6af23d3801f6b6f
Enrique Millan Valbuena <enrique.millanvalbuena@gmail.com>
2021-11-08 17:17:42 +0100

    BUG, CLN: Typos and units in the plots
      * CLN: Nondimensional results (FOM Calibration)
      * BUG: Nonlinear evaluation is done with u*
      * BUG: Typo in the BDF-2 coefficients
      * CLN: Add footnote and remove equation number

--- research_project/piston/figures/wiggles/plot.py
+++ research_project/piston/figures/wiggles/plot.py
@@ -9,14 +9,18 @@ seaborn.set_theme("paper")
 smooth = pd.read_csv("probes_FOM_17.csv", index_col=0)["0.0"].squeeze()
 wiggled = pd.read_csv("probes_FOM_18.csv", index_col=0)["0.0"].squeeze()
 
+a0_17 = 19.7
+a0_18 = 10.3
+
 fig, ax = plt.subplots(figsize=(7, 4))
 
-ax.plot(smooth.index, smooth.values, label="smooth", alpha=0.5)
-ax.plot(wiggled.index, wiggled.values, label="wiggled")
+ax.plot(smooth.index, smooth.values / a0_17, label="smooth", alpha=0.5)
+ax.plot(wiggled.index, wiggled.values / a0_18, label="wiggled")
 
 ax.grid(True)
 ax.legend()
 ax.set_xlabel("$t$ (s)")
-ax.set_ylabel("Outflow velocity (m/s)")
+ax.set_ylabel("$u / a_0$")
+ax.set_title("Outflow Velocity Comparison")
 
 plt.savefig("smooth_vs_wiggled.png", **FIG_KWARGS)
